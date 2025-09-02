bbc6459770243bb3f055fcd8b64205741fd62203
Cleomar12 <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>
2025-09-02 11:44:34 -0400

    Cria um layout inical para as views

--- testephp/app/views/dashboard.php
+++ testephp/app/views/dashboard.php
@@ -36,7 +36,7 @@
             background: #fff;
             padding: 20px;
             margin: 10px auto;
-            border-radius: 8px;
+            border-radius: 5px;
             box-shadow: 0 2px 5px rgba(0,0,0,.1);
             max-width: 600px;
         }
